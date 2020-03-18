77663245318685f86924220b2c311acbef281bae
Michael P. Jung <michael.jung@terreon.de>
2020-03-18 18:19:22 +0100

    Add working implementation of abort_n function

--- src/lib.rs
+++ src/lib.rs
@@ -2,42 +2,96 @@ use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-struct Aborted {
-    num_polls: usize
+#[derive(Debug)]
+pub struct Aborted {
+    pub num_polls: usize
 }
 
-struct AbortN<'a, T, F>
-where F: Future<Output=T>
+pub struct AbortN<T>
+where
+    T: Future
 {
     num_polls: usize,
     max_polls: usize,
-    future: Pin<&'a mut F>,
+    future: T,
 }
 
-impl<'a, T, F> Future for AbortN<'a, T, F>
-where F: Future<Output=T> {
-    type Output=Result<T, Aborted>;
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut me = Pin::into_inner(self);
-        if me.num_polls >= me.max_polls {
+impl<T> Future for AbortN<T>
+where
+    T: Future,
+{
+    type Output = Result<T::Output, Aborted>;
+    
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.num_polls >= self.max_polls {
             return Poll::Ready(Err(Aborted {
-                num_polls: me.num_polls
+                num_polls: self.num_polls
             }));
         }
-        me.num_polls += 1;
-        match me.future.poll(cx) {
-            Poll::Ready(v) => Poll::Ready(Ok(v)),
-            Poll::Pending => Poll::Pending
+        // Safety: we never move `self.future`
+        unsafe {
+            let mut num_polls = self.as_mut().map_unchecked_mut(|me| &mut me.num_polls);
+            *num_polls += 1;
+            let future = self.as_mut().map_unchecked_mut(|me| &mut me.future);
+            match future.poll(cx) {
+                Poll::Ready(v) => Poll::Ready(Ok(v)),
+                Poll::Pending => Poll::Pending
+            }
         }
     }
+}
+
+pub fn abort_n<T>(future: T, max_polls: usize) -> AbortN<T>
+where
+    T: Future,
+{
+    AbortN {
+        num_polls: 0,
+        max_polls,
+        future,
+    }
+}
+
+pub struct Never;
+
+impl Future for Never {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        cx.waker().wake_by_ref();
+        Poll::Pending
+    }
+}
+
+pub fn never() -> Never {
+    Never
 }
 
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use crate::{abort_n, never};
+
+    #[tokio::test]
+    async fn abort_n_0_err() {
+        assert!(abort_n(async { 42 }, 0).await.is_err());
     }
+
+    #[tokio::test]
+    async fn abort_n_1_ok() {
+        let result = abort_n(async { 42usize }, 1).await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 42usize);
+    }
+
+    #[tokio::test]
+    async fn abort_n_err() {
+        for max_polls in 0..100 {
+            let result = abort_n(async { never().await }, max_polls).await;
+            assert!(result.is_err());
+            assert_eq!(result.unwrap_err().num_polls, max_polls);
+        }
+    }
+
 }
 
